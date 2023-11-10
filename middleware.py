f0341087aa2413f6847713d8100a0957e9a9eac4
navyad <navyad.pro@gmail.com>
2023-11-10 13:58:55 +0530

    check places for product and request id attrs

---
+++ middleware.py
@@ -0,0 +1,33 @@
+import json
+
+from django.http import JsonResponse
+
+
+class CheckRequiredAttributesMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.method == "POST":
+            post_data = json.loads(request.body.decode('utf-8'))
+            product_data = post_data.get("productData")
+            product_id = product_data.get("productId")
+            product_network_id = product_data.get("productNetworkId")
+
+            if not product_id or not product_network_id:
+                response_data = {'error': 'productId and productNetworkId are required'}
+                return JsonResponse(response_data, status=400)
+            
+            from ocen.utils import Constansts
+            if product_id != Constansts.PRODUCT_ID or Constansts.PRODUCT_NETWORK_ID != 'NTX_BPNL':
+                error_message = 'Invalid values for productId or productNetworkId.'
+                return JsonResponse({'error': error_message}, status=400)
+            
+            request_id = post_data.get("requestId")
+            if not request_id:
+                return JsonResponse({'error': "requestId is required"}, status=400)
+
+
+        response = self.get_response(request)
+        return response
+
--- ocen/utils.py
+++ ocen/utils.py
@@ -3,3 +3,11 @@ import json
 def to_json(byte_data):
     json_string = byte_data.decode('utf-8')
     return json.loads(json_string)
+
+
+class Constansts:
+    LOAN_AGENT_BASE_URL = "http://localhost:8000"
+    LENDER_A_BASE_URL = "http://localhost:8001"
+    LENDER_B_BASE_URL = "http://localhost:8002"
+    PRODUCT_ID = "BNPL23"
+    PRODUCT_NETWORK_ID = "NTX_BPNL" 
